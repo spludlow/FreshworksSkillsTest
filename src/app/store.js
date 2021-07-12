1ab04415e770b0c9fc05779345115b57aeaf1f0a
spludlow <spludlow@hotmail.com>
2021-07-12 16:16:53 -0700

    remove counter

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import { jobSkillsApi } from "../features/jobSkills/job-skills-api-slice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     [jobSkillsApi.reducerPath]: jobSkillsApi.reducer,
   },
   middleware: (getDefaultMiddleware) => {
