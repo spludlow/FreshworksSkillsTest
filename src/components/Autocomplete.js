c64a3d7594695cded6b717e58168a550b005c619
spludlow <spludlow@hotmail.com>
2021-07-12 22:23:47 -0700

    autocomplete focus checks

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -1,8 +1,9 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import "../App.css";
 import debounce from "lodash.debounce";
 
 function Autocomplete({ label, selected, setSelected, query }) {
+  const wrapperRef = useRef(null);
   const [value, setValue] = useState(
     selected ? selected?.title || selected?.suggestion : ""
   );
@@ -10,6 +11,20 @@ function Autocomplete({ label, selected, setSelected, query }) {
   const [focused, setFocused] = useState(false);
 
   const { data = [], isFetching, error } = query(contains, { skip: !contains });
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        setFocused(false);
+      }
+    }
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [wrapperRef]);
 
   const debouncedSearch = useCallback(
     debounce((nextValue) => setContains(nextValue), 250),
@@ -29,13 +44,14 @@ function Autocomplete({ label, selected, setSelected, query }) {
   };
 
   return (
-    <div className="autocomplete">
+    <div className="autocomplete" ref={wrapperRef}>
       <div className="input">
         <label>{label}</label>
         <input
           className="autocomplete-input"
           value={value}
           onChange={handleInputChange}
+          onFocus={() => setFocused(true)}
         />
       </div>
       {focused && (
